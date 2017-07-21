94043055ca1ed33cc7722a978cfe66b3f4a99de6
Vinit Singh <dev.vinit18@gmail.com>
2017-07-21 11:40:39 +0530

    ToDo's list is being displayed

--- js/main.js
+++ js/main.js
@@ -4,4 +4,27 @@ var ToDo = angular.module('ng-ToDo' , []);
 
 ToDo.controller('mainController' ,function($scope){
     console.log($scope);
+    $scope.ListOfToDo = [
+        {
+            title : "Do my Homework" ,
+            details : "I must do my homework"
+        },
+        {
+            title : "Do my Homework" ,
+            details : "I must do my homework"
+        },
+        {
+            title : "Do my Homework" ,
+            details : "I must do my homework"
+        },
+        {
+            title : "Do my Homework" ,
+            details : "I must do my homework"
+        },
+        {
+            title : "Do my Homework" ,
+            details : "I must do my homework"
+        }
+    ];
+    
 });
